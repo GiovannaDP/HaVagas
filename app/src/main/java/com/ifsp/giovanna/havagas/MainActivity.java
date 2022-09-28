f5f0682a3e96c89d1db4a15b8077102f1d6a4fc2
Giovanna Danelli <giovannadanellipau@gmail.com>
2022-09-27 21:33:19 -0300

    Salvar as informacoes que estao na tela ao rotacionar a tela

--- app/src/main/java/com/ifsp/giovanna/havagas/MainActivity.java
+++ app/src/main/java/com/ifsp/giovanna/havagas/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ifsp.giovanna.havagas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -13,6 +14,21 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding activityMainBinding;
     private Formulario formulario;
+    private final static String EDIT_TEXT_NOME = "EDIT_TEXT_NOME";
+    private final static String EDIT_TEXT_EMAIL = "EDIT_TEXT_EMAIL";
+    private final static String CHECKBOX = "CHECKBOX";
+    private final static String SPINNER_TELEFONE = "SPINNER_TELEFONE";
+    private final static String EDIT_TEXT_TELEFONE = "EDIT_TEXT_TELEFONE";
+    private final static String EDIT_TEXT_CELULAR = "EDIT_TEXT_CELULAR";
+    private final static String SEXO = "SEXO";
+    private final static String EDIT_TEXT_DATANASCIMENTO = "EDIT_TEXT_DATANASC";
+    private final static String SPINNER_FORMACAO = "EDIT_TEXT_FORMACAO";
+    private final static String EDIT_TEXT_ANOFORMATURA = "EDIT_TEXT_ANOFORMATURA";
+    private final static String EDIT_TEXT_ANOCONCLUSAO = "EDIT_TEXT_ANOCONCLUSAO";
+    private final static String EDIT_TEXT_INSTITUICAO = "EDIT_TEXT_INSTITUICAO";
+    private final static String EDIT_TEXT_TITULOMONOGRAFIA = "EDIT_TEXT_TITULOMONOGRAFIA";
+    private final static String EDIT_TEXT_ORIENTADOR = "EDIT_TEXT_ORIENTADOR";
+    private final static String EDIT_TEXT_VAGASINTERESSE = "EDIT_TEXT_VAGASINTERESSE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,5 +111,99 @@ public class MainActivity extends AppCompatActivity {
 
             formulario = null;
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(EDIT_TEXT_NOME, activityMainBinding.nomeEt.getText().toString());
+        outState.putString(EDIT_TEXT_EMAIL, activityMainBinding.emailEt.getText().toString());
+        outState.putString(CHECKBOX, activityMainBinding.checkBox.isChecked() ? "Sim" : "Não");
+        outState.putString(SPINNER_TELEFONE, activityMainBinding.telefoneSp.getSelectedItem().toString());
+        outState.putString(EDIT_TEXT_TELEFONE, activityMainBinding.telefoneEt.getText().toString());
+        outState.putString(EDIT_TEXT_CELULAR, activityMainBinding.telefoneCelularEt.getText().toString());
+        outState.putString(SEXO, activityMainBinding.femininoRb.isChecked() ? "Feminino" : "Masculino");
+        outState.putString(SPINNER_FORMACAO, activityMainBinding.formacaoSp.getSelectedItem().toString());
+        outState.putString(EDIT_TEXT_ANOFORMATURA, activityMainBinding.anoDeFormaturaEt.getText().toString());
+        outState.putString(EDIT_TEXT_ANOCONCLUSAO, activityMainBinding.anoDeFormaturaEt.getText().toString());
+        outState.putString(EDIT_TEXT_INSTITUICAO, activityMainBinding.instituicaoEt.getText().toString());
+        outState.putString(EDIT_TEXT_TITULOMONOGRAFIA, activityMainBinding.tituloDeMonografiaEt.getText().toString());
+        outState.putString(EDIT_TEXT_ORIENTADOR, activityMainBinding.orientadorEt.getText().toString());
+        outState.putString(EDIT_TEXT_VAGASINTERESSE, activityMainBinding.vagasdeInteresseEt.getText().toString());
+        outState.putString(EDIT_TEXT_DATANASCIMENTO, activityMainBinding.dataNascimentoEt.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        activityMainBinding.nomeEt.setText(savedInstanceState.getString(EDIT_TEXT_NOME));
+        activityMainBinding.emailEt.setText(savedInstanceState.getString(EDIT_TEXT_EMAIL));
+
+        if(savedInstanceState.getString(CHECKBOX).equals("Sim")){
+            activityMainBinding.checkBox.setChecked(true);
+        }else{
+            activityMainBinding.checkBox.setChecked(false);
+        }
+
+        if(savedInstanceState.getString(SPINNER_TELEFONE).equals("Residencial")){
+            activityMainBinding.telefoneSp.setSelection(0);
+        }else{
+            activityMainBinding.telefoneSp.setSelection(1);
+        }
+
+        activityMainBinding.telefoneEt.setText(savedInstanceState.getString(EDIT_TEXT_TELEFONE));
+        activityMainBinding.telefoneCelularEt.setText(savedInstanceState.getString(EDIT_TEXT_CELULAR));
+
+        if(savedInstanceState.getString(SPINNER_TELEFONE).equals("")){
+            activityMainBinding.adicionarCelularBt.setVisibility(View.VISIBLE);
+            activityMainBinding.telefoneCelularLl.setVisibility(View.GONE);
+            activityMainBinding.removerCelularBt.setVisibility(View.GONE);
+
+        }else {
+            activityMainBinding.telefoneCelularLl.setVisibility(View.VISIBLE);
+            activityMainBinding.removerCelularBt.setVisibility(View.VISIBLE);
+            activityMainBinding.adicionarCelularBt.setVisibility(View.GONE);
+        }
+
+        if(savedInstanceState.getString(SEXO).equals("Feminino")){
+            activityMainBinding.femininoRb.setChecked(true);
+        }else{
+            activityMainBinding.masculinoRb.setChecked(true);
+        }
+
+        activityMainBinding.dataNascimentoEt.setText(savedInstanceState.getString(EDIT_TEXT_DATANASCIMENTO));
+
+        switch (savedInstanceState.getString(SPINNER_FORMACAO)){
+            case "Fundamental":
+                activityMainBinding.formacaoSp.setSelection(0);
+                break;
+            case "Médio":
+                activityMainBinding.formacaoSp.setSelection(1);
+                break;
+            case "Graduação":
+                activityMainBinding.formacaoSp.setSelection(2);
+                break;
+            case "Especialização":
+                activityMainBinding.formacaoSp.setSelection(3);
+                break;
+            case "Mestrado":
+                activityMainBinding.formacaoSp.setSelection(4);
+                break;
+            case "Doutorado":
+                activityMainBinding.formacaoSp.setSelection(5);
+                break;
+            default:
+                activityMainBinding.formacaoSp.setSelection(0);
+                break;
+        }
+
+        activityMainBinding.anoDeFormaturaEt.setText(savedInstanceState.getString(EDIT_TEXT_ANOFORMATURA));
+        activityMainBinding.anoDeConclusaoEt.setText(savedInstanceState.getString(EDIT_TEXT_ANOCONCLUSAO));
+        activityMainBinding.instituicaoEt.setText(savedInstanceState.getString(EDIT_TEXT_INSTITUICAO));
+        activityMainBinding.tituloDeMonografiaEt.setText(savedInstanceState.getString(EDIT_TEXT_TITULOMONOGRAFIA));
+        activityMainBinding.orientadorEt.setText(savedInstanceState.getString(EDIT_TEXT_ORIENTADOR));
+        activityMainBinding.vagasdeInteresseEt.setText(savedInstanceState.getString(EDIT_TEXT_VAGASINTERESSE));
     }
 }
